df156944264a8a5708dee1d3c9e0b6ec9edd3c5f
HusseinSaad <hussein.saad2021@gmail.com>
2023-11-10 00:00:59 +0200

    Initial commit

---
+++ models/author.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+  first_name: {
+    type: String,
+    required: true,
+    maxlength: 100,
+  },
+  family_name: {
+    type: String,
+    required: true,
+    maxlength: 100,
+  },
+  date_of_birth: {
+    type: Date,
+  },
+  date_of_death: {
+    type: Date,
+  },
+});
+
+AuthorSchema.virtual("name").get(function () {
+  let fullname = "";
+  if (this.first_name && this.family_name)
+    fullname = this.family_name + ", " + this.first_name;
+
+  return fullname;
+});
+
+AuthorSchema.virtual("url").get(function () {
+  return "/catalog/author/" + this._id;
+});
+
+AuthorSchema.virtual("lifespan").get(function () {
+  let lifetime_string = "";
+  if (this.date_of_birth) {
+    lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+      DateTime.DATE_MED
+    );
+  }
+  lifetime_string += " - ";
+  if (this.date_of_death) {
+    lifetime_string += DateTime.fromJSDate(this.date_of_death).toLocaleString(
+      DateTime.DATE_MED
+    );
+  }
+  return lifetime_string;
+});
+
+module.exports = mongoose.model("Author", AuthorSchema);
